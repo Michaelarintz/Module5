8ef002e2999ef2d07e3415ce76bf40a6d4dae60b
Carsten Rintz <carstenrintz@Carstens-iMac.Home>
2020-03-28 20:42:29 +0000

    new code.

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ var scriptElement = document.createElement('script'); scriptElement.nonce = 'thi
 });
 
 (function (global) {
-  
+
 
 var dc = {};
 
@@ -84,6 +84,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl, buildAndShowHomeHTML,
+},
    // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
@@ -117,8 +118,8 @@ var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", "'" +
-  chosenCategoryShortName + "'");
+var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", " " +
+  chosenCategoryShortName + " ");
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
